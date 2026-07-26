eaacf24b6d27542b582142d354db65c9db7f18cb
Priya Singh <prishorastudio@gmail.com>
2026-07-26 17:07:01 +0530

    Allow Phase 3 publishing mode override

--- services/blogger.js
+++ services/blogger.js
@@ -3,12 +3,12 @@ const { google } = require("googleapis");
 const { authorizeGoogle } = require("../auth/googleAuth");
 const { buildArticleHtml } = require("./htmlBuilder");
 
-
 async function publishToBlogger({
   article,
   seo,
   imageUrl,
   imageData,
+  isDraft = publisher.blogger.createAsDraft,
 }) {
   const auth = await authorizeGoogle();
 
@@ -26,8 +26,7 @@ async function publishToBlogger({
 
   const response = await blogger.posts.insert({
     blogId: publisher.blogger.blogId,
-    isDraft: publisher.blogger.createAsDraft,
-    
+    isDraft,
     requestBody: {
       title: seo.seoTitle,
       content: htmlContent,
